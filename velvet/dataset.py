26c07faea90c565900924bf89c84a99f39f26262
dinhanhx <dinhanhx@gmail.com>
2023-05-31 21:08:24 +0700

    Implement EVJVQA dataset

---
+++ velvet/dataset.py
@@ -0,0 +1,44 @@
+import json
+from pathlib import Path
+
+from torch.utils.data import Dataset
+
+
+class EVJVQA(Dataset):
+    def __init__(self, root_dir: Path) -> None:
+        """
+        Parameters
+        ----------
+        root_dir : Path
+            Directory where contain EVJVQA train-images/, lang_evjvqa_train.json
+        """
+        self.root_dir = root_dir
+        self.img_dir = root_dir.joinpath("train-images")
+        self.json_file = root_dir.joinpath("lang_evjvqa_train.json")
+
+        self.dataset = json.load(open(self.json_file))["annotations"]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index: int) -> dict:
+        """
+        Parameters
+        ----------
+        index : int
+            index in the json file
+
+        Returns
+        -------
+        dict
+            {'id': 0,
+            'image_id': 2301,
+            'question': 'what color is the shirt of the girl wearing glasses?',
+            'answer': 'the girl wearing glasses wears a red shirt',
+            'language': 'en',
+            'image_file': PosixPath('/storage/anhvd/data/EVJVQA/train-images/00000002301.jpg')}
+        """
+        data = self.dataset[index]
+        fmt_image_id = str(data["image_id"]).zfill(11)
+        data["image_file"] = list(self.img_dir.glob(f"{fmt_image_id}.*"))[0]
+        return data
