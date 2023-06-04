7b684b7102cadaa665c3f41610e1d30e9dcf4de5
dinhanhx <dinhanhx@gmail.com>
2023-06-04 15:13:48 +0700

    Add regex_match functions

--- velvet/instruction_template.py
+++ velvet/instruction_template.py
@@ -1,4 +1,5 @@
-from typing import Any, Tuple
+import re
+from typing import Any, Tuple, Union
 
 
 class InstructionTemplate:
@@ -11,6 +12,10 @@ class InstructionTemplate:
     def make_instruction_response_pair(*args: Any, **kwds: Any) -> Tuple[str, str]:
         return "", ""
 
+    @staticmethod
+    def regex_match(text: str) -> Union[re.Match, None]:
+        return re.Match()
+
 
 class VisualQuestionAnswerTemplate(InstructionTemplate):
     @staticmethod
@@ -21,6 +26,11 @@ class VisualQuestionAnswerTemplate(InstructionTemplate):
             f"Generate answer in {language}: {question}",
             f"{answer}",
         )
+
+    @staticmethod
+    def regex_match(text: str) -> Union[re.Match, None]:
+        pattern = r"^Generate answer in ([a-zA-Z]{2}):"
+        return re.match(pattern, text)
 
 
 class ReverseVisualQuestionAnswerTemplate(InstructionTemplate):
@@ -33,11 +43,21 @@ class ReverseVisualQuestionAnswerTemplate(InstructionTemplate):
             f"{question}",
         )
 
+    @staticmethod
+    def regex_match(text: str) -> Union[re.Match, None]:
+        pattern = r"^Generate question in ([a-zA-Z]{2}):"
+        return re.match(pattern, text)
+
 
 class ImageCaptionTemplate(InstructionTemplate):
     @staticmethod
     def make_instruction_response_pair(caption: str, language: str) -> Tuple[str, str]:
         return f"Generate caption in {language}:", f"{caption}"
+
+    @staticmethod
+    def regex_match(text: str) -> Union[re.Match, None]:
+        pattern = r"^Generate caption in ([a-zA-Z]{2}):"
+        return re.match(pattern, text)
 
 
 class CompleteImageCaptionTemplate(InstructionTemplate):
@@ -50,6 +70,11 @@ class CompleteImageCaptionTemplate(InstructionTemplate):
             f"{tail_caption}",
         )
 
+    @staticmethod
+    def regex_match(text: str) -> Union[re.Match, None]:
+        pattern = r"^Complete caption in ([a-zA-Z]{2}):"
+        return re.match(pattern, text)
+
 
 class TranslateTemplate(InstructionTemplate):
     @staticmethod
@@ -61,6 +86,11 @@ class TranslateTemplate(InstructionTemplate):
             f"{target_text}",
         )
 
+    @staticmethod
+    def regex_match(text: str) -> Union[re.Match, None]:
+        pattern = r"^Translate to ([a-zA-Z]{2}):"
+        return re.match(pattern, text)
+
 
 class ImageTextMatchTemplate(InstructionTemplate):
     @staticmethod
@@ -68,3 +98,8 @@ class ImageTextMatchTemplate(InstructionTemplate):
         caption: str, language: str, label: bool
     ) -> Tuple[str, str]:
         return f"Is this a pair in {language}: {caption}", f"{label}"
+
+    @staticmethod
+    def regex_match(text: str) -> Union[re.Match, None]:
+        pattern = r"^Is this a pair in ([a-zA-Z]{2}):"
+        return re.match(pattern, text)
