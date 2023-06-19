36ccead0643180ee8ef314b31f263825a608487f
dinhanhx <dinhanhx@gmail.com>
2023-06-19 13:47:48 +0000

    Add textcaps dataset

--- velvet/dataset/__init__.py
+++ velvet/dataset/__init__.py
@@ -5,5 +5,6 @@ from torch.utils.data import Dataset
 from .cc_sbu_align import CCSBUAlign
 from .coco import COCO
 from .evjvqa import EVJVQA
+from .textcaps import TextCaps
 
-UnionVelvetDataset = Union[Dataset, CCSBUAlign, COCO, EVJVQA]
+UnionVelvetDataset = Union[Dataset, CCSBUAlign, COCO, EVJVQA, TextCaps]
---
+++ velvet/dataset/textcaps.py
@@ -0,0 +1,86 @@
+import json
+import random
+from pathlib import Path
+from typing import Type, Union
+
+from torch.utils.data import Dataset
+
+from velvet.instruction_template import ImageCaptionTemplate, InstructionTemplate
+from velvet.iso639_1 import iso639_1_list
+
+
+class TextCaps(Dataset):
+    def __init__(
+        self,
+        image_root_dir: Path,
+        metadata_root_dir: Path,
+        iso639_1_code: str,
+        shuffle_seed: int = None,
+        template_class: Type[
+            Union[InstructionTemplate, ImageCaptionTemplate]
+        ] = InstructionTemplate,
+    ) -> None:
+        """
+        Parameters
+        ----------
+        image_root_dir : Path
+            Directory where contain OpenImages train_images/
+        metadata_root_dir : Path
+            Directory where contain TextCaps-vi en/, vi/...
+        iso639_1_code : str
+            Language code following ISO 639-1
+        shuffle_seed : int, optional
+            if shuffle_seed is None, don't shuffle dataset else shuffle according to seed value, by default None
+        template_class : Type[ Union[InstructionTemplate, ImageCaptionTemplate] ], optional
+            Template to create instruction response pair, by default InstructionTemplate
+        """
+        assert any(
+            [iso639_1_code == i.part1 for i in iso639_1_list]
+        ), f"{iso639_1_code} is not a valid ISO 639-1 code"
+        self.iso639_1_code = iso639_1_code
+
+        self.image_root_dir = image_root_dir
+        self.image_dir = image_root_dir.joinpath("train_images")
+
+        self.meta_root_dir = metadata_root_dir
+        self.json_file = metadata_root_dir.joinpath(
+            f"{iso639_1_code}/TextCaps_0.1_train.json"
+        )
+        assert (
+            self.json_file.is_file()
+        ), f"{iso639_1_code}/TextCaps_0.1_train.json does not exist"
+
+        self.dataset = json.load(open(self.json_file, "r", encoding="utf-8"))["data"]
+        if shuffle_seed is not None:
+            random.seed(shuffle_seed)
+            random.shuffle(self.dataset)
+
+        self.template_class = template_class
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index) -> dict:
+        """
+        Parameters
+        ----------
+        index : int
+            index in the json file
+
+        Returns
+        -------
+        dict
+            See https://textvqa.org/textcaps/dataset/#:~:text=suggestions%20and%20feedback.-,Description,-TextCaps%20JSON%20files
+            and https://huggingface.co/datasets/dinhanhx/TextCaps-vi
+        """
+        data = self.dataset[index]
+        image_file = self.image_dir.joinpath(data["image_id"] + ".jpg")
+        data["image_file"] = image_file
+
+        instruction, response = self.template_class.make_instruction_response_pair(
+            caption=data["caption_str"],
+            language=self.iso639_1_code,
+        )
+        data["_instruction_"] = instruction
+        data["_response_"] = response
+        return data
