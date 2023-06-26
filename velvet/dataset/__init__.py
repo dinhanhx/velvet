b40d5a02bf22b71802dcf025bec4804750732e8c
dinhanhx <dinhanhx@gmail.com>
2023-06-26 08:42:31 +0000

    Update docs **and** refactor

--- velvet/dataset/__init__.py
+++ velvet/dataset/__init__.py
@@ -34,21 +34,23 @@ def create_all_dataset_list(config_data_dir_toml_file: str, seed: int = 1312) ->
     ----------
     config_data_dir_toml_file : str
         Path in form of str points to data_dir.toml
+    seed : int
+        Random seed, by default 1312
 
     Returns
     -------
     list
         List of dict. Each dict contain:
-            - `d_key`: a valid dataset in toml file
+            - `d_name`: a valid dataset in toml file
             - `d_lang`: a valid iso639-1 code
             - `d_object`: a object of dataset class
     """
     config_data_dir = toml.load(open(config_data_dir_toml_file, "r"))
 
-    ic_dataset_keys = ["coco", "textcaps"]
+    ic_dataset_names = ["coco", "textcaps"]
     ic_dataset_classes = [COCO, TextCaps]
 
-    vqa_dataset_keys = ["okvqa", "textvqa", "vqav2"]
+    vqa_dataset_names = ["okvqa", "textvqa", "vqav2"]
     vqa_dataset_classes = [OKVQA, TextVQA, VQAv2]
 
     mixed_lang = "mixed"
@@ -58,7 +60,7 @@ def create_all_dataset_list(config_data_dir_toml_file: str, seed: int = 1312) ->
 
     dataset_list.append(
         {
-            "d_key": "evjvqa",
+            "d_name": "evjvqa",
             "d_lang": mixed_lang,
             "d_object": EVJVQA(
                 Path(config_data_dir["evjvqa"]["root_dir"]),
@@ -70,7 +72,7 @@ def create_all_dataset_list(config_data_dir_toml_file: str, seed: int = 1312) ->
 
     dataset_list = dataset_list + [
         {
-            "d_key": "gcc",
+            "d_name": "gcc",
             "d_lang": lang,
             "d_object": GCC(
                 Path(config_data_dir["gcc"]["gcc_vi_dir"]),
@@ -83,37 +85,66 @@ def create_all_dataset_list(config_data_dir_toml_file: str, seed: int = 1312) ->
         for lang in langs
     ]
 
-    for d_key, d_class in zip(ic_dataset_keys, ic_dataset_classes):
+    for d_name, d_class in zip(ic_dataset_names, ic_dataset_classes):
         for lang in langs:
             d_object = d_class(
-                Path(config_data_dir[d_key]["image_root_dir"]),
-                Path(config_data_dir[d_key]["metadata_root_dir"]),
+                Path(config_data_dir[d_name]["image_root_dir"]),
+                Path(config_data_dir[d_name]["metadata_root_dir"]),
                 lang,
                 seed,
                 ImageCaptionTemplate,
             )
-            dataset_list.append({"d_key": d_key, "d_lang": lang, "d_object": d_object})
+            dataset_list.append({"d_name": d_name, "d_lang": lang, "d_object": d_object})
 
-    for d_key, d_class in zip(vqa_dataset_keys, vqa_dataset_classes):
+    for d_name, d_class in zip(vqa_dataset_names, vqa_dataset_classes):
         for lang in langs:
             d_object = d_class(
-                Path(config_data_dir[d_key]["image_root_dir"]),
-                Path(config_data_dir[d_key]["metadata_root_dir"]),
+                Path(config_data_dir[d_name]["image_root_dir"]),
+                Path(config_data_dir[d_name]["metadata_root_dir"]),
                 lang,
                 seed,
                 VisualQuestionAnswerTemplate,
             )
-            dataset_list.append({"d_key": d_key, "d_lang": lang, "d_object": d_object})
+            dataset_list.append({"d_name": d_name, "d_lang": lang, "d_object": d_object})
 
     return dataset_list
 
 
-def order_dataset_list(dataset_list: list) -> list:
-    order_key = ["okvqa", "textcaps", "textvqa", "coco", "vqav2", "gcc", "evjvqa"]
-    order_lang = ["en", "vi", "mixed"]
+def order_dataset_list(
+    dataset_list: list,
+    order_name: Union[list, None] = None,
+    order_lang: Union[list, None] = None,
+) -> list:
+    """Order the list of all datasets that is the output of `create_all_dataset_list()`
+
+    This is a kinda unstable function when comes to extensibility.
+    One is advised to read the whole function before using.
+
+    Parameters
+    ----------
+    dataset_list : list
+        the list of all datasets that is returned by `create_all_dataset_list()`
+    order_name : Union[list, None], optional
+        List of valid dataset in toml file, by default ["okvqa", "textcaps", "textvqa", "coco", "vqav2", "gcc", "evjvqa"]
+    order_lang : Union[list, None], optional
+        List of valid iso639-1 code, by default ["en", "vi", "mixed"]
+
+    Returns
+    -------
+    list
+        List of dict. Each dict contain:
+            - `d_name`: a valid dataset in toml file
+            - `d_lang`: a valid iso639-1 code
+            - `d_object`: a object of dataset class
+    """
+    if order_name is None:
+        order_name = ["okvqa", "textcaps", "textvqa", "coco", "vqav2", "gcc", "evjvqa"]  # This is very hard
+        # order_name = ["gcc", "vqav2", "coco", "textvqa", "textcaps", "okvqa", "evjvqa"]  # This is less hard
+    if order_lang is None:
+        order_lang = ["en", "vi", "mixed"]
 
     def order_criteria(item: dict):
-        return order_key.index(item["d_key"]), order_lang.index(item["d_lang"])
+        return order_name.index(item["d_name"]), order_lang.index(item["d_lang"])
 
     dataset_list.sort(key=order_criteria)
     return dataset_list
@@ -133,6 +164,29 @@ class PadDataset(Dataset):
 def pad_dataset_list(
     dataset_list: list, num_devices=8, batch_size=8, accumulation_step=8
 ) -> list:
+    """Add extra dataset so the total length of all dataset in the list
+    is divided by num_devices * batch_size * accumulation_step
+
+
+    Parameters
+    ----------
+    dataset_list : list
+        the list of all datasets that is returned by `create_all_dataset_list()` or `order_dataset_list()`
+    num_devices : int, optional
+        obvious, by default 8
+    batch_size : int, optional
+        obvious, by default 8
+    accumulation_step : int, optional
+        obvious, by default 8
+
+    Returns
+    -------
+    list
+        List of dict. Each dict contain:
+            - `d_name`: a valid dataset in toml file
+            - `d_lang`: a valid iso639-1 code
+            - `d_object`: a object of dataset class
+    """
     total_length = 0
     for i in dataset_list:
         total_length += len(i["d_object"])
@@ -151,7 +205,7 @@ def pad_dataset_list(
         {
             "d_object": pad_dataset,
             "d_lang": last_dataset["d_lang"],
-            "d_key": last_dataset["d_key"],
+            "d_name": last_dataset["d_name"],
         }
     )
     return dataset_list
